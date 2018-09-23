b3419de05536e9ce927dfdde0cf56ff73ad0b798
gogbajbobo <grigoriev.maxim@gmail.com>
2018-09-23 12:28:04 +0300

    add StreamHandler to log to console

--- storage/__init__.py
+++ storage/__init__.py
@@ -6,22 +6,33 @@ app = Flask(__name__)
 
 
 def logger_setup():
+
+    from logging import StreamHandler
     from logging.handlers import RotatingFileHandler
+
+    log_level = logging.DEBUG
+    formatter = logging.Formatter("%(asctime)s - %(name)s - [LINE:%(lineno)d]# - %(levelname)s - %(message)s")
+
+    def configured_log_handler(log_handler):
+        log_handler.setLevel(log_level)
+        log_handler.setFormatter(formatter)
+        return log_handler
 
     logs_path = os.path.join('logs', 'storage.log')
     if not os.path.exists(os.path.dirname(logs_path)):
         os.makedirs(os.path.dirname(logs_path))
 
-    app.logger.setLevel(logging.DEBUG)
-    file_handler = RotatingFileHandler(logs_path, maxBytes=512000, backupCount=1)
-    file_handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - [LINE:%(lineno)d]# - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
+    file_handler = configured_log_handler(RotatingFileHandler(logs_path, maxBytes=512000, backupCount=1))
+    stream_handler = configured_log_handler(StreamHandler())
+
     app.logger.addHandler(file_handler)
+    app.logger.addHandler(stream_handler)
 
     log = logging.getLogger('werkzeug')
-    log.setLevel(logging.DEBUG)
+    log.setLevel(log_level)
+
     log.addHandler(file_handler)
+    log.addHandler(stream_handler)
+
 
 logger_setup()
-
