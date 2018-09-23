97ac62487461bfc44a3d0e391b821257eba69bba
gogbajbobo <grigoriev.maxim@gmail.com>
2018-09-23 12:58:44 +0300

    load configuration from YOURAPPLICATION_SETTINGS envvar

--- storage/__init__.py
+++ storage/__init__.py
@@ -3,6 +3,7 @@ import logging
 
 from flask import Flask
 app = Flask(__name__)
+app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 
 
 def logger_setup():
@@ -10,7 +11,9 @@ def logger_setup():
     from logging import StreamHandler
     from logging.handlers import RotatingFileHandler
 
-    log_level = logging.DEBUG
+    is_debug = app.config['DEBUG']
+
+    log_level = logging.DEBUG if is_debug else logging.INFO
     formatter = logging.Formatter("%(asctime)s - %(name)s - [LINE:%(lineno)d]# - %(levelname)s - %(message)s")
 
     def configured_log_handler(log_handler):
@@ -22,17 +25,17 @@ def logger_setup():
     if not os.path.exists(os.path.dirname(logs_path)):
         os.makedirs(os.path.dirname(logs_path))
 
-    file_handler = configured_log_handler(RotatingFileHandler(logs_path, maxBytes=512000, backupCount=1))
-    stream_handler = configured_log_handler(StreamHandler())
-
-    app.logger.addHandler(file_handler)
-    app.logger.addHandler(stream_handler)
-
     log = logging.getLogger('werkzeug')
     log.setLevel(log_level)
 
+    file_handler = configured_log_handler(RotatingFileHandler(logs_path, maxBytes=512000, backupCount=1))
+    app.logger.addHandler(file_handler)
     log.addHandler(file_handler)
-    log.addHandler(stream_handler)
+
+    if is_debug:
+        stream_handler = configured_log_handler(StreamHandler())
+        app.logger.addHandler(stream_handler)
+        log.addHandler(stream_handler)
 
 
 logger_setup()
