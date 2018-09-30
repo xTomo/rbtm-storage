e423d56ba911553bf9268bcf300bf585d24213dd
gogbajbobo <grigoriev.maxim@gmail.com>
2018-09-30 16:31:24 +0300

    mv routes error handlers in separate file

--- storage/__init__.py
+++ storage/__init__.py
@@ -8,26 +8,9 @@ app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 with app.app_context():
     logger.logger_setup()
 
-    from storage import views
+    import views, routes.errors as re
 
     app.register_blueprint(views.bp_experiments)
     app.register_blueprint(views.bp_storage)
+    re.setup_error_handlers()
 
-
-# for returning error as json file
-@app.errorhandler(404)
-def not_found(exception):
-    app.logger.exception(exception)
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-@app.errorhandler(400)
-def incorrect_format(exception):
-    app.logger.exception(exception)
-    return make_response(jsonify({'error': 'Incorrect format'}), 400)
-
-
-@app.errorhandler(500)
-def incorrect_format(exception):
-    app.logger.exception(exception)
-    return make_response(jsonify({'error': 'Internal Server'}), 500)
---
+++ storage/routes/errors.py
@@ -0,0 +1,20 @@
+from flask import current_app as app
+from flask import make_response, jsonify
+
+
+def setup_error_handlers():
+    # for returning error as json file
+    @app.errorhandler(404)
+    def not_found(exception):
+        app.logger.exception(exception)
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    @app.errorhandler(400)
+    def incorrect_format(exception):
+        app.logger.exception(exception)
+        return make_response(jsonify({'error': 'Incorrect format'}), 400)
+
+    @app.errorhandler(500)
+    def incorrect_format(exception):
+        app.logger.exception(exception)
+        return make_response(jsonify({'error': 'Internal Server'}), 500)
