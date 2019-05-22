31a9a9791ffbee483e41cadec7dd00b4b4d229f8
Alexey Buzmakov <buzmakov@gmail.com>
2019-05-22 17:22:33 +0300

    Code clean up

--- storage/rewrite.py
+++ storage/rewrite.py
@@ -19,36 +19,34 @@ def rewrite_file_h5(file):
     logger.debug(experiment_id)
     new_data_path = os.path.abspath(os.path.join(os.path.dirname(file), experiment_id))
     if not os.path.exists(new_data_path):
-        data = h5py.File(os.path.abspath(os.path.join(os.path.dirname(file), experiment_id)))
+        with h5py.File(os.path.abspath(os.path.join(os.path.dirname(file), experiment_id))) as  data:
+            experiments = db['experiments']
+            experiment_info = dumps(experiments.find({"_id": experiment_id}))
+            data.attrs.create("exp_info", experiment_info.encode('utf8'))
 
-        experiments = db['experiments']
-        experiment_info = dumps(experiments.find({"_id": experiment_id}))
-        data.attrs.create("exp_info", experiment_info.encode('utf8'))
+            data.create_group("empty")
+            data.create_group("dark")
+            data.create_group("data")
+            logger.debug("hdf5: created groups")
 
-        data.create_group("empty")
-        data.create_group("dark")
-        data.create_group("data")
-        logger.debug("hdf5: created groups")
+            frames = db['frames']
+            for frame_id in old_data.keys():
+                frame_info = dumps(frames.find({"_id": ObjectId(frame_id)}))
+                json_frame = loads(frame_info)
+                frame_number = str(json_frame[0]['frame']['number'])
+                frame_type = str(json_frame[0]['frame']['mode'])
+                frame_dataset = data[frame_type].create_dataset(frame_number, data=old_data[frame_id], compression="gzip", compression_opts=9)
+                frame_dataset.attrs.create("frame_info", frame_info.encode('utf8'))
 
-        frames = db['frames']
-        for frame_id in old_data.keys():
-            frame_info = dumps(frames.find({"_id": ObjectId(frame_id)}))
-            json_frame = loads(frame_info)
-            frame_number = str(json_frame[0]['frame']['number'])
-            frame_type = str(json_frame[0]['frame']['mode'])
-            frame_dataset = data[frame_type].create_dataset(frame_number, data=old_data[frame_id], compression="gzip", compression_opts=9)
-            frame_dataset.attrs.create("frame_info", frame_info.encode('utf8'))
-
-        print("hdf5: wrote compressed datasets")
-        logger.debug("hdf5: wrote compressed datasets")
-        data.flush()
-        data.close()
+            print("hdf5: wrote compressed datasets")
+            logger.debug("hdf5: wrote compressed datasets")
+            data.flush()
+            data.close()
     else:
         logger.debug("file already exists")
 
 
 experiments_path = os.path.join('data', 'experiments')
 for root, dirs, files in os.walk(experiments_path):
-    for name in files:
-       if name == 'frames.h5':
-           rewrite_file_h5(os.path.join(root, name))
+    for 'frames.h5' in files:
+        rewrite_file_h5(os.path.join(root, name))
